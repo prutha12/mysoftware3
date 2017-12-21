0247ee958da7b1d5606dafae4e2bad5773702dcf
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-21 14:41:13 +0530

    routes updated with html

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'usename' : 'prutha'}
+    user = {'usename': 'prutha'}
     return '''
 <html>
-<head>
- <title> web app page</title>
-</head>
-<body>
-   <h1> hello , ''' + user['username'] + ''' </h1>
-</body>
+  <head>
+    <title> web app page</title>
+  </head>
+  <body>
+   <h1> hello , ''' + user['usename'] + ''' </h1>
+  </body>
 </html>'''
