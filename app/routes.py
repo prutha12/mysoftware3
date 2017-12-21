df43b4c682bbf34a3a8f4bedd8032517378eb9bb
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-21 14:56:57 +0530

    all latest files

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,9 @@
+from flask import Flask
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'usename': 'prutha'}
-    return '''
-<html>
-  <head>
-    <title> web app page</title>
-  </head>
-  <body>
-   <h1> hello , ''' + user['usename'] + ''' </h1>
-  </body>
-</html>'''
+    title = {'my web app page'}
+    return render_template('index.html',title=title,user=user)
