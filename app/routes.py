07e52d407ef618e806f36f0ca6850c774fb0a60a
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-22 10:17:05 +0530

    with routes modified added login part

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,12 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html',title='Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
