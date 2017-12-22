048d74a5582a8c7d297185bff7b703f99f968f7f
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-22 11:37:08 +0530

    commit *

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ def login():
     if form.validate_on_submit():
         flash('Login request for user{}, remember_me={}',format(form.username.data, form.remember_me.data))
     return redirect('/index')
- return render_template('login.html',title='Sign In',form=form)
+    return render_template('login.html',title='Sign In',form=form)
 
 @app.route('/')
 @app.route('/index')
