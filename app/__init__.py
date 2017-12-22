3271278e4e899a1db52487d3cef631969441045b
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-22 13:10:15 +0530

    with fb and migration commit copy1

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import Config
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
