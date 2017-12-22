769acdaae110a1fcefbd7695e0acb97f3d77c147
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-22 11:26:01 +0530

    new files of login edit

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app.forms import LoginForm
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user{}, remember_me={}',format( form.username.data, form.remember_me.data))
+        flash('Login request for user{}, remember_me={}',format(form.username.data, form.remember_me.data))
     return redirect('/index')
  return render_template('login.html',title='Sign In',form=form)
 
