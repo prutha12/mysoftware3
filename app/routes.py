a21a783b19e3ec01fc0fd360c6b30f3e0c902dac
Prutha Kalangutkar <prutha.kalangutkar4@gmail.com>
2017-12-22 12:26:14 +0530

    reps files copy 1

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
-from flask import render_template,flash,redirect
+from flask import render_template,flash,redirect,url_for
 from app import app
 from app.forms import LoginForm
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user{}, remember_me={}',format(form.username.data, form.remember_me.data))
-    return redirect('/index')
+        flash('Login request for user{}, remember_me={}'.format(
+             form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',title='Sign In',form=form)
 
 @app.route('/')
